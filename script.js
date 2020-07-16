1ef83c80b37547cc1074d185f993e42e3be34919
paulwilliams <paalpwmd>
2020-07-15 20:21:05 -0500

    update

--- script.js
+++ script.js
@@ -1,38 +1,31 @@
 // TODO: add code here
-
 window.addEventListener('load', function () {
-  let container = document.querySelector('div#container');
-  let count = document.querySelector('p#astronautCount');
+  let container = document.getElementById('container');
+  let count = document.getElementById('astronautCount');
   fetch('https://handlers.education.launchcode.org/static/astronauts.json')
     .then((response) => response.json())
     .then((data) => {
-      data.sort((a, b) => (a.hoursInSpace < b.hoursInSpace ? 1 : -1));
-      count.innerHTML = `Total Astronaut Count: ${data.length}`;
-      data.forEach((astronaut) => {
-        console.log(data);
+      data.sort(
+        (astronaut1, astronaut2) =>
+          astronaut1.hoursInSpace < astronaut2.hoursInSpace
+      );
+      count.innerHTML = `There are ${data.length} astronauts`;
+      for (item in data) {
         container.innerHTML += `<div class="astronaut">
-                                    <div class="bio">
-                                        <h3>${astronaut.firstName} ${
-          astronaut.lastName
-        }</h3>
-                                        <ul>
-                                            <li>Hours in space: ${
-                                              astronaut.hoursInSpace
-                                            }</li>
-                                            <li ${
-                                              astronaut.active
-                                                ? 'style="color: green"'
-                                                : 'style="color: red"'
-                                            }>Active: ${astronaut.active}</li>
-                                            <li>Skills: ${astronaut.skills.map(
-                                              (skill) => ` ${skill}`
-                                            )}</li>
-                                        </ul>
-                                    </div>
-                                    <img class="avatar" src=${
-                                      astronaut.picture
-                                    }>
-                                </div>`;
-      });
+          <div class="bio">
+            <h3>${data[item].firstName} ${data[item].lastName}</h3>
+            <ul>
+              <li>Hours in space: ${data[item].hoursInSpace}</li>
+              <li style="${
+                data[item].active ? 'color: green;' : 'color: red'
+              }">Active: ${data[item].active}</li>
+              <li>Skills: ${data[item].skills.map((skill) => {
+                return ` ${skill}`;
+              })}</li>
+            </ul>
+          </div>
+          <img class="avatar" src="${data[item].picture}">
+</div>`;
+      }
     });
 });
